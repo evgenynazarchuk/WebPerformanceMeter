59dc6cd32f2c7f9d8d27ce7e14d05327877b1d3e
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-08-08 16:50:28 +0300

    fix test

--- PerformanceTests/Tests/Scenarios/SampleTest1.cs
+++ PerformanceTests/Tests/Scenarios/SampleTest1.cs
@@ -10,19 +10,19 @@ namespace PerformanceTests.Tests.Scenarios
 {
     public class SampleTest1
     {
+        [PerformanceTest(1, 200)]
         [PerformanceTest(10, 200)]
+        [PerformanceTest(30, 200)]
         [PerformanceTest(60, 200)]
-        [PerformanceTest(30 * 60, 200)]
-        [PerformanceTest(60 * 60, 200)]
-        [PerformanceTest(90 * 60, 200)]
-        public async Task TwoParallelUsers(int seconds, int usersCount)
+        [PerformanceTest(90, 200)]
+        public async Task TwoParallelUsers(int minutes, int usersCount)
         {
             // Arange
             var app = new WebApplication();
             var user1 = new TestWaitUser1(app.HttpClient);
             var user2 = new TestWaitUser2(app.HttpClient);
-            var plan1 = new ActiveUsersOnPeriod(user1, usersCount, seconds.Seconds());
-            var plan2 = new ActiveUsersOnPeriod(user2, usersCount, seconds.Seconds());
+            var plan1 = new ActiveUsersOnPeriod(user1, usersCount, minutes.Minutes());
+            var plan2 = new ActiveUsersOnPeriod(user2, usersCount, minutes.Minutes());
 
             // Run test
             await new Scenario()
--- PerformanceTests/Tests/Scenarios/SampleTest2.cs
+++ PerformanceTests/Tests/Scenarios/SampleTest2.cs
@@ -9,15 +9,19 @@ namespace PerformanceTests.Tests.Scenarios
 {
     public class SampleTest2
     {
-        [PerformanceTest]
-        public async Task FewActs()
+        [PerformanceTest(1, 200)]
+        [PerformanceTest(10, 200)]
+        [PerformanceTest(30, 200)]
+        [PerformanceTest(60, 200)]
+        [PerformanceTest(90, 200)]
+        public async Task FewActs(int minutes, int usersCount)
         {
             var app = new WebApplication();
             var user1 = new TestWaitUser1(app.HttpClient);
             var user2 = new TestWaitUser2(app.HttpClient);
-            var plan1 = new ActiveUsersOnPeriod(user1, 200, 2.Minutes());
-            var plan2 = new ActiveUsersOnPeriod(user2, 200, 2.Minutes());
-            var plan3 = new ActiveUsersOnPeriod(user1, 200, 2.Minutes());
+            var plan1 = new ActiveUsersOnPeriod(user1, usersCount, minutes.Minutes());
+            var plan2 = new ActiveUsersOnPeriod(user2, usersCount, minutes.Minutes());
+            var plan3 = new ActiveUsersOnPeriod(user1, usersCount, minutes.Minutes());
 
             await new Scenario()
                 .AddSequentialPlans(plan1)
